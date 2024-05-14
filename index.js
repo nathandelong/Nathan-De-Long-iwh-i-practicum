856422ff2811e5619d0b89b903b66cc5acd0258f
nathandelong <ndelong@hubspot.com>
2024-05-14 16:37:12 -0400

    Updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 
-const PRIVATE_APP_ACCESS = 'MY_TOKEN';
+const PRIVATE_APP_ACCESS = 'MY_TO KEN';
 
 // Render the update page and pass the email value to the template
 app.get('/update', (req, res) => {
